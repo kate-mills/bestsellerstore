5f50f6eff9fc7487f8fda3eb132400c8da177ecb
Kate Mills <kate.mills1124@gmail.com>
2021-03-21 10:34:31 -0700

    Filter Context error

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,50 +1,12 @@
 /* eslint-disable jsx-a11y/no-onchange */
 import React from 'react'
-import { useFilterContext } from '../context/filter_context'
-import { BsFillGridFill, BsList } from 'react-icons/bs'
 import styled from 'styled-components'
 const Sort = () => {
-  const {
-    filtered_items: items,
-    grid_view,
-    setGridView,
-    setListView,
-    updateSort,
-    sort
-  } = useFilterContext()
   return (
     <Wrapper>
       <div className="btn-container">
-        <button
-          type="button"
-          className={`${grid_view ? 'active': null}`}
-          onClick={setGridView}
-        ><BsFillGridFill />
-        </button>
-        <button
-          type="button"
-          className={`${!grid_view ? 'active': null}`}
-          onClick={setListView}
-        > <BsList/>
-        </button>
+        Sort
       </div>
-      <p>{items.length} items found</p>
-      <hr/>
-      <form>
-        <label htmlFor='sort'>sort by</label>
-        <select
-          name="sort"
-          id="sort"
-          className="sort-input"
-          value={sort}
-          onChange={updateSort}
-        >
-          <option value="price-lowest"> price (lowest)</option>
-          <option value="price-highest"> price (highest)</option>
-          <option value="name-a"> name (a-z)</option>
-          <option value="name-z"> name (z-a)</option>
-        </select>
-      </form>
     </Wrapper>
   )
 }
