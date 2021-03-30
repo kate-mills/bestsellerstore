1bd764f198a387400c4720381f42d14d0a1385b3
Kate Mills <kate.mills1124@gmail.com>
2021-03-30 13:41:52 -0700

    FinalForm - get info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
+  //FinalForm - get info
   return (
     <Wrapper>
       <div className='section-center'>
