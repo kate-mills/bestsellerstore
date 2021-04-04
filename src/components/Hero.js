71b1feec5d9bcb60b5ad4344a4cd6f89a3fe12a2
Kate Mills <kate.mills1124@gmail.com>
2021-04-04 08:40:14 -0700

    clean Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -44,62 +44,65 @@ const Hero = () => {
 }
 
 const Wrapper = styled.section`
-  min-height: 60vh;
   display: grid;
+  min-height: 60vh;
   place-items: center;
   .img-container {
     display: none;
   }
   p {
-    line-height: 2;
-    max-width: 45em;
-    margin-bottom: 2rem;
     color: var(--clr-grey-5);
     font-size: 1rem;
+    line-height: 2;
+    margin-bottom: 2rem;
+    max-width: 45em;
+  }
+  h1 {
+    margin-top: 2rem;
   }
   @media (min-width: 992px) {
-    height: calc(100vh - 5rem);
-    grid-template-columns: 1fr 1fr;
     gap: 8rem;
+    grid-template-columns: 1fr 1fr;
+    height: calc(100vh - 5rem);
     h1 {
-      margin-bottom: 2rem;
+      margin-top: 1rem;
     }
     p {
       font-size: 1.25rem;
     }
     .hero-btn {
-      padding: 0.75rem 1.5rem;
       font-size: 1rem;
+      padding: 0.75rem 1.5rem;
     }
     .img-container {
       display: block;
       position: relative;
     }
     .main-img {
-      width: 100%;
-      height: 550px;
-      position: relative;
       border-radius: var(--radius);
       display: block;
+      height: 550px;
       object-fit: cover;
+      position: relative;
+      width: 100%;
     }
     .accent-img {
-      position: absolute;
+      border-radius: var(--radius);
       bottom: 0;
       left: 0;
-      width: 250px;
+      position: absolute;
       transform: translateX(-50%);
-      border-radius: var(--radius);
+      width: 250px;
     }
     .img-container::before {
+      background: var(--clr-primary-9);
+      border-radius: var(--radius);
+      bottom: 0%;
       content: '';
+      height: 80%;
+      left: -8%;
       position: absolute;
       width: 10%;
-      height: 80%;
-      background: var(--clr-primary-9);
-      bottom: 0%;
-      left: -8%;
-      border-radius: var(--radius);
     }
   }
 `
