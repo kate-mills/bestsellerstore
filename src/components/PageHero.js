ba8549fdd9bda8eeb62a1aa1e4700072dc3c2f3c
Kate Mills <kate.mills1124@gmail.com>
2021-04-03 19:05:13 -0700

    bree font

--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -10,7 +10,7 @@ const PageHero = ({ title, shop, isLongName=false, showCount }) => {
       <div className="section-center">
         <h3><Link to="/">Home</Link>
           {shop && <Link to="/shop">/ Shop</Link> }
-          /<span className={`${isLongName? 'title long': 'title'}`}> {title}
+          /<span className="title"> {title}
           </span>
         </h3>
       </div>
@@ -22,14 +22,17 @@ const PageHero = ({ title, shop, isLongName=false, showCount }) => {
 const Wrapper = styled.section`
   background: var(--clr-primary-9);
   margin-top: 0.125rem;
+  font-family: 'bree' !important;
+  font-weight; 300 !important;
   width: 100%;
   min-height: 20vh;
   display: flex;
   align-items: center;
-
   color: var(--clr-primary-1);
   a {
     color: var(--clr-primary-4d);
+    font-family: 'bree' !important;
+    font-weight; 300 !important;
     padding: 0.5rem;
     transition: var(--transition);
   }
@@ -38,9 +41,8 @@ const Wrapper = styled.section`
   }
   .title{
     font-size: inherit;
-  }
-  .long{
-    font-size: 1.5rem;
+    font-family: 'bree' !important;
+    font-weight; 300 !important;
   }
   .count{ margin-left: 1rem; }
   @media(max-width: 500px){
