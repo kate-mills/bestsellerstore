2b4135c95a483ff9a5ca7a80e2af42d58ac03ef4
Kate Mills <kate.mills1124@gmail.com>
2021-04-04 09:38:06 -0700

    Cleanup Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,39 +6,40 @@ const Footer = () => {
     <Wrapper>
       <h6>
         &copy;{new Date().getFullYear()}
-        <span className="proxima">SkincareWebstore</span>
+        <span className="cursive"> SkincareWebstore</span>
       </h6>
       <h6>All rights reserved</h6>
     </Wrapper>
-      <div className="ally">Built by <a href="https://allydigitalsolutions.com">Ally Digital Solutions</a></div>
+      <div className="ally"><a style={{fontFamily: 'var(--font-title)'}} href="https://allydigitalsolutions.com">Built By  Ally Digital Solutions</a></div>
     </>
   )
 }
 
 const Wrapper = styled.footer`
-  height: 5rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
   align-items: center;
   background: var(--clr-black);
+  display: flex;
+  flex-direction: column;
+  height: 5rem;
+  justify-content: center;
   text-align: center;
-  & span.proxima {
-    font-family: 'proxima-nova';
-    font-weight: 400; /*proxima*/
+  & span.cursive {
     color: var(--clr-primary-9);
+    font-family: var(--font-cursive);
+    font-size: 1.3rem;
     letter-spacing: 0px;
+    margin: 0 0.3rem;
     text-transform: capitalize;
   }
   h6 {
     color: var(--clr-white);
-    margin: 0.1rem;
-    text-transform: none;
+    font-family: var(--font-title);
+    font-size: 0.8rem;
+    font-weight: var(--font-title-weight); /* h6 */
     letter-spacing: var(--spacing);
     line-height: 1.25;
-    font-family: 'bree';
-    font-weight: 300; /*bree*/
-    font-size: 1rem;
+    margin: 0.1rem;
+    text-transform: none;
   }
   @media (min-width: 776px) {
     flex-direction: row;
